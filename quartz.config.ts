7a22cce7ea13a7b62e88bda52e699acc5671a21c
Kang-min Kim <rkdals0203@gmail.com>
2025-03-31 00:08:48 +0900

    fix: base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "blog-5lubioxit-rkdals0203s-projects.vercel.app",
+    baseUrl: "blog-ocbz71og9-rkdals0203s-projects.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
